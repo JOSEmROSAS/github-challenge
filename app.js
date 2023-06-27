3aefccd9727bcc389e98088cb64d01f703522308
TXT-85 <txt.hackathon@TXTs-MacBook-Pro-66.local>
2023-06-26 22:28:43 -0700

    git challenge

---
+++ app.js
@@ -0,0 +1,6 @@
+function lightBulbOn() {
+    document.getElementById("bulb").src="https://img.freepik.com/free-vector/realistic-light-bulb-with-electricity_23-2149129410.jpg?w=2000";
+}
+function lightBulbOff(){
+    document.getElementById("bulb").src="https://as2.ftcdn.net/v2/jpg/00/77/99/99/1000_F_77999958_tZYT7xWlSJewLgsMYY3CrAWvYITlWYTF.jpg";
+}
